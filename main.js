df4da43ad6e120ce138a2a501e2d1950e12c2440
Nasser <nasser.yosef98@gmail.com>
2022-09-14 21:42:26 +0300

    gg

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ fastify.get('/car', async (request, reply) => {
     return car
 })
 
-const port = process.env.$PORT || 3000;
+const port = process.env.$PORT || process.env.PORT || 3000;
 const host = process.env.NODE_ENV === 'production' ? '0.0.0.0' : '127.1';
 
 // Run the server!
